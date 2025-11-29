19e890f472f329ebb30e2a0fab49270eaba02077
Kirk Gopaul <gopaulkirk@gmail.com>
2025-11-29 12:05:11 -0400

    Add button to show current date and time

--- welcome.php
+++ welcome.php
@@ -1,1 +1,21 @@
+<?php
+$datetime = date("Y-m-d h:i:s A"); 
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Date & Time Button</title>
+</head>
+<body>
+
+<button onclick="showDateTime()">Show Current Date & Time</button>
+
+<script>
+function showDateTime() {
+    alert("Current Date & Time: <?php echo $datetime; ?>");
+}
+</script>
+
+</body>
+</html>
